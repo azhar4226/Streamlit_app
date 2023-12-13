22da5e3fd00b546800eb502cee3407941c141805
Azhar Quadri <76815055+azhar4226@users.noreply.github.com>
2023-12-13 23:22:32 +0530

    largest_number_app.py

--- largest_number_app.py
+++ largest_number_app.py
@@ -6,15 +6,37 @@ def find_largest_number(num1, num2, num3):
     return largest_number
 
 def main():
-    st.title("Find the Largest Number App")
+    st.title("Find the Largest Number")
 
-    num1 = st.number_input("Enter the first number:")
-    num2 = st.number_input("Enter the second number:")
-    num3 = st.number_input("Enter the third number:")
+    col1, col2, col3 = st.beta_columns(3)
+
+    with col1:
+        num1 = st.number_input("Enter the first number:")
+
+    with col2:
+        num2 = st.number_input("Enter the second number:")
+
+    with col3:
+        num3 = st.number_input("Enter the third number:")
 
     if st.button("Find Largest Number"):
         result = find_largest_number(num1, num2, num3)
         st.success(f"The largest number is: {result}")
 
+# def main():
+#     st.title("Find the Largest Number App")
+
+#     num1 = st.number_input("Enter the first number:")
+#     num2 = st.number_input("Enter the second number:")
+#     num3 = st.number_input("Enter the third number:")
+
+#     if st.button("Find Largest Number"):
+#         result = find_largest_number(num1, num2, num3)
+#         st.success(f"The largest number is: {result}")
+
 if __name__ == "__main__":
     main()
+
+
+
+
