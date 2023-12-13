f6c67c13f83d55c6d4208696f9849c1f242f66bb
Azhar Quadri <76815055+azhar4226@users.noreply.github.com>
2023-12-13 23:24:57 +0530

    Update largest_number_app.py

--- largest_number_app.py
+++ largest_number_app.py
@@ -23,16 +23,6 @@ def main():
         result = find_largest_number(num1, num2, num3)
         st.success(f"The largest number is: {result}")
 
-# def main():
-#     st.title("Find the Largest Number App")
-
-#     num1 = st.number_input("Enter the first number:")
-#     num2 = st.number_input("Enter the second number:")
-#     num3 = st.number_input("Enter the third number:")
-
-#     if st.button("Find Largest Number"):
-#         result = find_largest_number(num1, num2, num3)
-#         st.success(f"The largest number is: {result}")
 
 if __name__ == "__main__":
     main()
